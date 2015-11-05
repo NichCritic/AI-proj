426cb5efab455b2307511becc4a5a4691adbc834
Nicholas Aelick <Nicholas Aelick>
2015-11-04 22:05:13 -0500

    data

--- utils/normalizeUrls.py
+++ utils/normalizeUrls.py
@@ -45,18 +45,19 @@ if __name__ == '__main__':
 	# 		dirname = dirpath.split('/')[-1]
 	# 		output_data.append((dirname, url))
 
-
+	i = -1
 	for dirpath, dirnames, filenames in os.walk(mypath):
 		for filen in filenames:
 			label = dirpath.split('/')[-1]
 			if(filen != 'index.txt'):
-				output_data.append((label, './data/'+label+'/'+filen))			
+				output_data.append((i, 'data/'+label+'/'+filen))
+		i=i+1			
 
 	random.shuffle(output_data)
 
 
 	with open('index.txt', 'w') as f:
-		f.write('\n'.join([f+' '+l for l, f in output_data]))
+		f.write('\n'.join([f+' '+str(l) for l, f in output_data]))
 	#print(generate_url(0, 'test.jpg'))
 	# print(generate_url(1, 'test.jpg'))
 	# print(generate_url(26, 'test.jpg'))
